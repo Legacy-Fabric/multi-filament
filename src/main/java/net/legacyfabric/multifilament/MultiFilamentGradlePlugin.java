40c0e74bfda62c398db55af2497cdf9e8b277404
Cat Core <arthur.koalas@free.fr>
2024-08-14 22:00:07 +0200

    Port more tasks

--- src/main/java/net/legacyfabric/multifilament/MultiFilamentGradlePlugin.java
+++ src/main/java/net/legacyfabric/multifilament/MultiFilamentGradlePlugin.java
@@ -3,10 +3,9 @@ package net.legacyfabric.multifilament;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.fabricmc.filament.FilamentGradlePlugin;
+
 import net.fabricmc.filament.task.CombineUnpickDefinitionsTask;
-
 import net.legacyfabric.multifilament.task.FixedRemapUnpickDefinitionsTask;
-
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskContainer;
@@ -19,6 +18,7 @@ public class MultiFilamentGradlePlugin implements Plugin<Project> {
 		project.getPlugins().apply(FilamentGradlePlugin.class);
 
 		final MultiFilamentExtension extension = project.getExtensions().create("multiFilament", MultiFilamentExtension.class);
+		extension.createConfigurations();
 		extension.createTasks();
 
 		TaskContainer tasks = project.getTasks();
@@ -29,6 +29,10 @@ public class MultiFilamentGradlePlugin implements Plugin<Project> {
 			task.getInput().set(combineUnpickDefinitions.getOutput());
 			task.getSourceNamespace().set("named");
 			task.getTargetNamespace().set("intermediary");
+		});
+
+		project.afterEvaluate(p -> {
+			extension.createTasksLate();
 		});
 	}
 }
